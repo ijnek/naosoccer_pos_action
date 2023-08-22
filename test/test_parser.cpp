5d86f3b7f2eeb2b23de49283a00053c1db758b4f
ijnek <kenjibrameld@gmail.com>
2023-08-22 01:20:45 +0000

    fix up stiffness bug, and add tests
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -15,7 +15,10 @@
 #include <cmath>
 
 #include "gtest/gtest.h"
+#include "nao_lola_command_msgs/msg/joint_indexes.hpp"
 #include "../src/parser.hpp"
+
+using JointIndexes = nao_lola_command_msgs::msg::JointIndexes;
 
 TEST(TestParser, TestIncorrectPositionSize)
 {
@@ -27,6 +30,83 @@ TEST(TestParser, TestIncorrectPositionSize)
   EXPECT_FALSE(parseResult.successful);
 }
 
+TEST(TestParser, TestPositionsAndStiffnessesSize)
+{
+  std::vector<std::string> testString = {
+    "! 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 300",
+  };
+
+  auto parseResult = parser::parse(testString);
+  EXPECT_EQ(parseResult.keyFrames.at(0).stiffnesses.stiffnesses.size(), JointIndexes::NUMJOINTS);
+  EXPECT_EQ(parseResult.keyFrames.at(0).positions.positions.size(), JointIndexes::NUMJOINTS);
+}
+
+TEST(TestParser, TestIndexesFilled)
+{
+  std::vector<std::string> testString = {
+    "! 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 300",
+  };
+
+  auto parseResult = parser::parse(testString);
+  const auto & key_frame = parseResult.keyFrames.at(0);
+
+  // Position indexes
+  ASSERT_EQ(key_frame.positions.indexes.size(), JointIndexes::NUMJOINTS);
+  EXPECT_EQ(key_frame.positions.indexes.at(0), JointIndexes::HEADYAW);
+  EXPECT_EQ(key_frame.positions.indexes.at(1), JointIndexes::HEADPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(2), JointIndexes::LSHOULDERPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(3), JointIndexes::LSHOULDERROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(4), JointIndexes::LELBOWYAW);
+  EXPECT_EQ(key_frame.positions.indexes.at(5), JointIndexes::LELBOWROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(6), JointIndexes::LWRISTYAW);
+  EXPECT_EQ(key_frame.positions.indexes.at(7), JointIndexes::LHIPYAWPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(8), JointIndexes::LHIPROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(9), JointIndexes::LHIPPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(10), JointIndexes::LKNEEPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(11), JointIndexes::LANKLEPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(12), JointIndexes::LANKLEROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(13), JointIndexes::RHIPROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(14), JointIndexes::RHIPPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(15), JointIndexes::RKNEEPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(16), JointIndexes::RANKLEPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(17), JointIndexes::RANKLEROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(18), JointIndexes::RSHOULDERPITCH);
+  EXPECT_EQ(key_frame.positions.indexes.at(19), JointIndexes::RSHOULDERROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(20), JointIndexes::RELBOWYAW);
+  EXPECT_EQ(key_frame.positions.indexes.at(21), JointIndexes::RELBOWROLL);
+  EXPECT_EQ(key_frame.positions.indexes.at(22), JointIndexes::RWRISTYAW);
+  EXPECT_EQ(key_frame.positions.indexes.at(23), JointIndexes::LHAND);
+  EXPECT_EQ(key_frame.positions.indexes.at(24), JointIndexes::RHAND);
+
+  // Stiffness indexes
+  ASSERT_EQ(key_frame.stiffnesses.indexes.size(), JointIndexes::NUMJOINTS);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(0), JointIndexes::HEADYAW);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(1), JointIndexes::HEADPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(2), JointIndexes::LSHOULDERPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(3), JointIndexes::LSHOULDERROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(4), JointIndexes::LELBOWYAW);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(5), JointIndexes::LELBOWROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(6), JointIndexes::LWRISTYAW);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(7), JointIndexes::LHIPYAWPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(8), JointIndexes::LHIPROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(9), JointIndexes::LHIPPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(10), JointIndexes::LKNEEPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(11), JointIndexes::LANKLEPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(12), JointIndexes::LANKLEROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(13), JointIndexes::RHIPROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(14), JointIndexes::RHIPPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(15), JointIndexes::RKNEEPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(16), JointIndexes::RANKLEPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(17), JointIndexes::RANKLEROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(18), JointIndexes::RSHOULDERPITCH);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(19), JointIndexes::RSHOULDERROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(20), JointIndexes::RELBOWYAW);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(21), JointIndexes::RELBOWROLL);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(22), JointIndexes::RWRISTYAW);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(23), JointIndexes::LHAND);
+  EXPECT_EQ(key_frame.stiffnesses.indexes.at(24), JointIndexes::RHAND);
+}
+
 TEST(TestParser, TestParsePosition)
 {
   std::vector<std::string> testString = {
@@ -35,9 +115,23 @@ TEST(TestParser, TestParsePosition)
 
   auto parseResult = parser::parse(testString);
   ASSERT_TRUE(parseResult.successful);
-  ASSERT_EQ(parseResult.keyFrames.size(), 1);
-  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300);
+  ASSERT_EQ(parseResult.keyFrames.size(), 1u);
+  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300u);
   EXPECT_NEAR(parseResult.keyFrames.at(0).positions.positions.at(1), 90 * M_PI / 180.0, 0.0001);
+}
+
+TEST(TestParser, TestStiffnessIsOneByDefault)
+{
+  std::vector<std::string> testString = {
+    "! 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 300",
+  };
+
+  auto parseResult = parser::parse(testString);
+  const auto & keyFrameOneStiffnesses = parseResult.keyFrames.at(0).stiffnesses.stiffnesses;
+  ASSERT_EQ(keyFrameOneStiffnesses.size(), JointIndexes::NUMJOINTS);
+  for (unsigned i = 0; i < JointIndexes::NUMJOINTS; ++i) {
+    EXPECT_EQ(keyFrameOneStiffnesses.at(i), 1.0);
+  }
 }
 
 TEST(TestParser, TestParseStiffness)
@@ -49,8 +143,8 @@ TEST(TestParser, TestParseStiffness)
 
   auto parseResult = parser::parse(testString);
   ASSERT_TRUE(parseResult.successful);
-  ASSERT_EQ(parseResult.keyFrames.size(), 1);
-  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300);
+  ASSERT_EQ(parseResult.keyFrames.size(), 1u);
+  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300u);
   EXPECT_NEAR(parseResult.keyFrames.at(0).stiffnesses.stiffnesses.at(1), 0.2, 0.0001);
 }
 
@@ -65,7 +159,7 @@ TEST(TestParser, TestDuration)
 
   auto parseResult = parser::parse(testString);
   ASSERT_TRUE(parseResult.successful);
-  ASSERT_EQ(parseResult.keyFrames.size(), 2);
-  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300);
-  EXPECT_EQ(parseResult.keyFrames.at(1).t_ms, 600);
+  ASSERT_EQ(parseResult.keyFrames.size(), 2u);
+  EXPECT_EQ(parseResult.keyFrames.at(0).t_ms, 300u);
+  EXPECT_EQ(parseResult.keyFrames.at(1).t_ms, 600u);
 }
